2ce618f2edb4b0bdd30b2b79394be0277de51a06
RafaelImperium <rafaelcansan38@gmail.com>
2025-04-07 18:31:47 -0400

    Create script-global.js

---
+++ js/script-global.js
@@ -0,0 +1,9 @@
+// script-global.js
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Página carregada com sucesso!');
+
+    // Exemplo de como alterar idiomas
+    const trocarIdioma = (idioma) => {
+        alert(`Idioma alterado para: ${idioma}`);
+    };
+});
